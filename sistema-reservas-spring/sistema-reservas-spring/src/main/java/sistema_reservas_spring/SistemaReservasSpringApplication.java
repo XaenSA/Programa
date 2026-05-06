4ac898f18f7e99564a6ccdc2fb6239e83259a2ca
XaenSA <estebanpro1805@gmail.com>
2026-05-05 22:49:49 -0500

    Configuracion inicial del proyecto con spring boot y dependencias

---
+++ sistema-reservas-spring/sistema-reservas-spring/src/main/java/sistema_reservas_spring/SistemaReservasSpringApplication.java
@@ -0,0 +1,13 @@
+package sistema_reservas_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaReservasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaReservasSpringApplication.class, args);
+	}
+
+}
---
+++ sistema-reservas-spring/sistema-reservas-spring/src/test/java/sistema_reservas_spring/SistemaReservasSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package sistema_reservas_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaReservasSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
