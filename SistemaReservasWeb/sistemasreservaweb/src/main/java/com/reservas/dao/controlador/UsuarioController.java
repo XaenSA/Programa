34f053d8a7468a59bd12b979afe48be7c2af39d7
XaenSA <estebanpro1805@gmail.com>
2026-05-05 23:12:32 -0500

    Creación de UsuarioController con métodos CRUD y patrón MVC

---
+++ SistemaReservasWeb/sistemasreservaweb/src/main/java/com/reservas/dao/controlador/UsuarioController.java
@@ -0,0 +1,45 @@
+package com.reservas.controlador;
+
+import com.reservas.modelo.Usuario;
+import com.reservas.repositorio.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    
+    @GetMapping
+    public String listarUsuarios(Model model) {
+        model.addAttribute("listaUsuarios", usuarioRepository.findAll());
+        return "usuarios"; // Esto buscará un archivo llamado usuarios.html
+    }
+
+   
+    @GetMapping("/nuevo")
+    public String mostrarFormularioRegistro(Model model) {
+        model.addAttribute("usuario", new Usuario());
+        return "formulario_usuario"; // Esto buscará un archivo formulario_usuario.html
+    }
+
+    
+    @PostMapping
+    public String guardarUsuario(@ModelAttribute("usuario") Usuario usuario) {
+        usuarioRepository.save(usuario); // ¡Guarda o actualiza automáticamente!
+        return "redirect:/usuarios";
+    }
+
+   
+    @GetMapping("/eliminar/{id}")
+    public String eliminarUsuario(@PathVariable Integer id) {
+        usuarioRepository.deleteById(id);
+        return "redirect:/usuarios";
+    }
+}
